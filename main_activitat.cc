37eddf13e924d5819ff6fc14e9cca9bfad2f3e3f
DESKTOP-MR1A87A\Cano <ramon.canoaparicio@gmail.com>
2019-03-01 18:13:34 +0100

    Main created 'main_activity.cc'

---
+++ main_activitat.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "Comparsa.h"
+
+using namespace std;
+
+void creaComparses(vector<Comparses> &comparses)
+
+int main() {
+
+    vector<Comparsa> comparses;
+
+    creaComparses(comparses);
+
+    
+
+
+}
