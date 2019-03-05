1f6aadd47a9edc0398b184d648bf7dd9f232a84d
DESKTOP-MR1A87A\Cano <ramon.canoaparicio@gmail.com>
2019-03-05 18:13:59 +0100

    Main loop

--- main_activitat.cc
+++ main_activitat.cc
@@ -4,15 +4,70 @@
 
 using namespace std;
 
-void creaComparses(vector<Comparses> &comparses)
+void creaComparses(vector<Comparsa> &comparses) {
+  /* Pre: comparses es un vector de Comparsa */
+  /* Post: el vector comparses conté les comparses entrades per l'usuari */
+  int n;
+  cin >> n;
+  comparses.resize(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> comparses[i];
+  }
+}
+
+void mostrarComparsa(const vector<Comparsa> &comparses) {
+  /* Pre: vector amb les comparses */
+  /* Post: imprimeix pel canal estandar de sortida les dades de la comparsa
+  */
+}
+
+void afegirParella(vector<Comparsa> &comparses) {
+  /* Pre: vector amb les comparses */
+  /* Post: nova comparsa afegida al vector */
+}
+
+void mostrarTotesComparses(const vector<Comparsa> &comparses) {
+  /* Pre: vector amb les comparses */
+  /* Post: imprimeix pel canal estandar de sortida les dades de totes les comparses */
+}
+
+void mostrarEcocaramel(const vector<Comparsa> &comparses) {
+  /* Pre: vector amb les comparses */
+  /* Post: imprimeix pel canal estandar de sortida si la comparsa té ecocaramel */
+}
 
 int main() {
 
     vector<Comparsa> comparses;
+    int opcio;
 
     creaComparses(comparses);
 
-    
+    cin >> opcio;
+    while (opcio != -5) {
+      switch (opcio) {
+        case -1:
+          mostrarComparsa(comparses);
+          break;
+        case -2:
+          afegirParella(comparses);
+          break;
+        case -3:
+          mostrarTotesComparses(comparses);
+          break;
+        case -4:
+          mostrarEcocaramel(comparses);
+          break;
+        default:
+          cout << "Opcio no implementada" << endl;
+          break;
+      }
+      cin >> opcio;
+    }
+
+
+
+
 
 
 }
