3a36418a34d0f5264d326efdbb907be73862e29e
DESKTOP-MR1A87A\Cano <ramon.canoaparicio@gmail.com>
2019-03-06 08:16:43 +0100

    algunes funcions del main

--- main_activitat.cc
+++ main_activitat.cc
@@ -17,13 +17,46 @@ void creaComparses(vector<Comparsa> &comparses) {
 
 void mostrarComparsa(const vector<Comparsa> &comparses) {
   /* Pre: vector amb les comparses */
-  /* Post: imprimeix pel canal estandar de sortida les dades de la comparsa
-  */
+  /* Post: imprimeix pel canal estandar de sortida les dades de la comparsa */
+  string sigles;
+  unsigned int i = 0;
+  bool trobat;
+  cin >> sigles;
+
+  while(not trobat and i < comparses.size()) {
+    if (comparses[i].consultaSigles() == sigles) {
+      cout << comparses[i] << endl;
+      trobat = true;
+    }
+    ++i;
+  }
+
+  if (not trobat) cout << "No s'ha trobat la Comparsa " << sigles << endl;
 }
 
 void afegirParella(vector<Comparsa> &comparses) {
   /* Pre: vector amb les comparses */
-  /* Post: nova comparsa afegida al vector */
+  /* Post: nova parella afegida a la comparsa */
+  string sigles;
+  unsigned int i = 0;
+  bool trobat;
+  cin >> sigles;
+
+  while(not trobat and i < comparses.size()) {
+    if (comparses[i].consultaSigles() == sigles) {
+      trobat = true;
+    }
+    else {
+      ++i;
+    }
+  }
+
+  if (not trobat) cout << "No s'ha trobat la Comparsa " << sigles << endl;
+  else {
+    Parella p;
+    cin >> p;
+    comparses[i].afegeixParella(p);
+  }
 }
 
 void mostrarTotesComparses(const vector<Comparsa> &comparses) {
